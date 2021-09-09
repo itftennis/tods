1f3d1c17a7015d51177e2ac5fde918bb640ef792
Rajeev Shenoy <itft.github@itftennis.com>
2021-09-09 13:36:36 +0100

    Created XmlType for ScheduleItems

--- src/Tennis-Open-Data-Standards/ScheduleItem.cs
+++ src/Tennis-Open-Data-Standards/ScheduleItem.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
+using Tennis_Open_Data_Standards.Attributes;
 
 namespace Tennis_Open_Data_Standards
 {
+    [NoUnboundCustom]
+    [XmlRoot("ScheduleItems"), XmlType(TypeName = "ScheduleItems")]
+    public class ScheduleItems
+    {
+        [XmlElement(IsNullable = false)]
+        public Collection<ScheduleItem> ScheduleItem { get; set; }
+    }
+
     public class ScheduleItem : CommonElements
     {
         //XML minOccurs=1 to 1
@@ -15,7 +25,7 @@ namespace Tennis_Open_Data_Standards
         //XML minOccurs=0 to 1
         public DateTime? StartDate { get; set; }
         //XML minOccurs=0 to 1
-        public DateTime? EndDate { get; set; }                
+        public DateTime? EndDate { get; set; }
         public string LocalTimeZone { get; set; }
         public string Description { get; set; }
     }
